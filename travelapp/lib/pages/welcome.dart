28490150ccd4e973ce8bc1b572306c38cbd05c0d
rensvg <rensvgemert@gmail.com>
2024-03-01 16:59:00 +0100

    Welcome page update
    Background image changed

--- travelapp/lib/pages/welcome.dart
+++ travelapp/lib/pages/welcome.dart
@@ -7,45 +7,50 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/background-get-started.jpg"),
+              fit: BoxFit.cover,
+              opacity: 0.6),
+        ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            const SizedBox(
-              height: 60,
-            ),
-            Image.asset(
-              'assets/welcome_plane.png',
-              width: 350,
-              height: 350,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            const Text(
-              'Explore your journey with us',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 36),
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HomePage(),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  color: Colors.white,
+                  child: const Text(
+                    'Document your journey anywhere in the world',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 33),
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 231, 163, 17),
-                  foregroundColor: Colors.white,
-                  fixedSize: const Size(200, 20),
-                  textStyle: const TextStyle(
-                      fontSize: 25, fontWeight: FontWeight.bold)),
-              child: const Text(
-                'Get Started',
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 43, 43, 43),
+                    foregroundColor: Colors.white,
+                    fixedSize: const Size(200, 20),
+                    textStyle: const TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.bold)),
+                child: const Text(
+                  'Get Started',
+                ),
               ),
             ),
           ],
